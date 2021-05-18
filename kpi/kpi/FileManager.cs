22cc71dfb9ffcf9c77932bc0bd9e89897127eccc
Marichka <marichka.v.richtsi@gmail.com>
2021-05-18 23:24:11 +0300

    Add FileManager and User

---
+++ kpi/kpi/FileManager.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace kpi
+{
+    public class FileManager
+    {
+        private string path;
+
+        public FileManager(string path)
+        {
+            this.path = path;
+        }
+
+        public void Register(User newUser)
+        {
+            using (StreamWriter sw = new StreamWriter(path+@"\users.txt", true))
+            {
+                sw.WriteLine(newUser.access+" "+newUser.login+" "+newUser.password);
+            }
+        }
+
+        public bool Avtorization(User user)
+        {
+            string usersInfo = user.access + " " + user.login + " " + user.password;
+            using (StreamReader sr = new StreamReader(path+@"\users.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    if (sr.ReadLine() == usersInfo)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+        
+    }
+}
---
+++ kpi/kpi/User.cs
@@ -0,0 +1,16 @@
+﻿namespace kpi
+{
+    public class User
+    {
+        public readonly string login;
+        public readonly string password;
+        public readonly string access;
+
+        public User(string n, string p, string a)
+        {
+            login = n;
+            password = p;
+            access = a;
+        }
+    }
+}
