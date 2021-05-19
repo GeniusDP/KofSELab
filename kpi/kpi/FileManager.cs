dd8722b6bdac65efce221eeabf83d189cd6b7146
Marichka <marichka.v.richtsi@gmail.com>
2021-05-19 11:43:30 +0300

    Add CreateTest and AppendToLecture methods

--- kpi/kpi/FileManager.cs
+++ kpi/kpi/FileManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace kpi
 {
@@ -36,6 +37,36 @@ namespace kpi
             return false;
         }
         
-        
+        public void CreateTest(int numberOfTest, string nameOfCourse, int countOfQuestions){
+            using (StreamWriter sw = new StreamWriter(path+@"\"+nameOfCourse+@"\Test"+numberOfTest+".txt"))
+            {
+                for (int i = 0; i < countOfQuestions; i++)
+                {
+                    sw.WriteLine(Console.ReadLine());// запитання
+                    sw.WriteLine(Console.ReadLine());// варіант відповіді 1
+                    sw.WriteLine(Console.ReadLine());// варіант відповіді 2
+                    sw.WriteLine(Console.ReadLine());// варіант відповіді 3
+                    sw.WriteLine(Console.ReadLine());// варіант відповіді 4
+                    sw.WriteLine(Console.ReadLine());// відповідь
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter(path + @"\" + nameOfCourse + @"\StatisticTest" + numberOfTest + ".txt"))
+            {
+            }
+        }
+
+        public void AppendToLecture(int numberOfLecture, string nameOfCourse)
+        {
+            using (StreamWriter sw = new StreamWriter(path+@"\"+nameOfCourse+@"\Lecture"+numberOfLecture+".txt", true))
+            {
+                while (true)
+                {
+                    string str = Console.ReadLine();
+                    if(str=="0") break;
+                    sw.WriteLine();
+                }
+            }
+        }
     }
 }
